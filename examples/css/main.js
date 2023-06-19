1a32f4942ef040024a965715638fcc00ea60f6fc
Aditya Subramanyam <subramanyam963@gmail.com>
2023-06-19 23:18:14 +0530

    corrected examples

--- examples/css/main.js
+++ examples/css/main.js
@@ -1,4 +1,4 @@
-import styles from 'import-map-utils/css/loader.js?src=./main.css&base=http://localhost:8081/examples/css/';
+import styles from 'import-map-utils/css/loader.js?src=./main.css';
 import './test.js';
 
 document.getElementById('main').innerText = 'I am main';
